0331a2de91b563d7fd2ad3b6b45aaf04df57656e
Glen <ultrapancake@gmail.com>
2019-09-22 15:23:47 -0600

    created function to populate array into buttons.

--- assets/java-script/app.js
+++ assets/java-script/app.js
@@ -1,10 +1,30 @@
 // array of animals; Will push user input to this array with on click function
-var arr = ["monkey", "donkey", "cat", "dog", "chicken", "cow", "pig"]
+var topics = ["monkey", "donkey", "cat", "dog", "chicken", "cow", "pig"]
+
+// populate starting topics on load
+buttonDiv();
+
+// function to populate topics  array into buttons and append them to the button-div
+function buttonDiv(){
+    $("#button-div").empty()
+    for(var i = 0; i < topics.length; i++){
+        var button = $("<button>");
+        button.addClass('gif-button');
+        button.attr('data-id', topics[i]);
+        button.text(topics[i]);
+        $("#button-div").append(button);
+    }
+}
 
 // on click function for submit button to push user input to array
 $("#submit-button").on("click", function () {
-    var input = $("#search-term").val();
-    arr.push(input);
-    console.log(arr);
-    // INSERT function that populates buttons from array
+    event.preventDefault();
+    // creates user input value into a variable; replaces any spaces with an underscore
+    var input = $("#search-term").val().replace(/ /g, "_");
+    // push the input variable to the array
+    topics.push(input);
+    // console log the array to check user input at end of array
+    console.log(topics);
+    // --------- INSERT function that populates buttons from array -------
+    buttonDiv();
 })
